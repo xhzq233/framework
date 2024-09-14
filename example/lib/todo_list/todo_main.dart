26d3058225ae0e8bbcb053771901b9eef08f3567
shinxiahou <shinxiahou@tencent.com>
2024-09-14 15:11:32 +0800

    Rm list

--- example/lib/todo_list/todo_main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:example/todo_list/todo_page.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MaterialApp(
-    color: Colors.blue,
-    onGenerateRoute: (settings) => TodoListPage.route(),
-  ));
-}
--- lib/list.dart
+++
@@ -1,4 +0,0 @@
-library list;
-
-export 'src/list/base_input_field.dart';
-export 'src/list/base_list.dart';
