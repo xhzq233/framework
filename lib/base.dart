19e31651c28de2936df2faabff7e2ada5bbd3cfa
shinxiahou <shinxiahou@tencent.com>
2024-10-07 17:23:36 +0800

    Update

--- lib/base.dart
+++ lib/base.dart
@@ -1,6 +1,6 @@
 library base;
 
-export 'src/base/app_life_cycle_listener.dart';
+export 'src/base/network_connectivity_provider.dart';
 export 'src/base/device.dart';
 export 'src/base/paging_action.dart';
 export 'src/base/router_observer.dart';
